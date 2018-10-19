ec4b8f922506af4a59d28dca2238b278a6e70485
vlkong <viulong.kong@fr.ibm.com>
2018-10-19 16:18:25 +0200

    2.8.125

--- docs/mp/_static/bizstyle.js
+++ docs/mp/_static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("DOcplex.MP: Mathematical Programming Modeling for Python V2.7 documentation");
+        $("li.nav-item-0 a").text("DOcplex.MP: Mathematical Programming Modeling for Python V2.8 documentation");
     }
 });
