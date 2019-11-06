66f3d46a780b3e2fa9d0df0884dc5659ac759cc6
Viu Long Kong <viulong.kong@fr.ibm.com>
2019-11-06 16:04:36 +0100

    2.11.176

--- docs/cp/_static/documentation_options.js
+++ docs/cp/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'V2.10',
+    VERSION: 'V2.11',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
