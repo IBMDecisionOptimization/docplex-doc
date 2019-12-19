41cb95893f743f9e898370311232f7ecda9832bb
Viu Long Kong <viulong.kong@fr.ibm.com>
2019-12-19 22:08:07 +0100

    2.12.182

--- docs/cp/_static/bizstyle.js
+++ docs/cp/_static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("DOcplex.CP: Constraint Programming Modeling for Python V2.11 documentation");
+        $("li.nav-item-0 a").text("DOcplex.CP: Constraint Programming Modeling for Python V2.12 documentation");
     }
 });
