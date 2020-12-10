7386cb97eb9e85259ff797febd81028212f17385
Viu-Long Kong <viulong.kong@fr.ibm.com>
2020-12-10 17:07:04 +0100

    update for release 2.19.202

--- docs/cp/_static/documentation_options.js
+++ docs/cp/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'V2.18',
+    VERSION: 'V2.19',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
