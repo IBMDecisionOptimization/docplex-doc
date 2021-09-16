39c2f4fac33eb5423acc5173aed63acdbd1aef4d
Viu-Long Kong <viulong.kong@fr.ibm.com>
2021-09-16 09:37:12 +0200

    updated to 2.22

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'V2.21',
+    VERSION: 'V2.22',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
--- docs/mp/_static/bizstyle.js
+++ docs/mp/_static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("DOcplex.MP: Mathematical Programming Modeling for Python V2.21 documentation");
+        $("li.nav-item-0 a").text("DOcplex.MP: Mathematical Programming Modeling for Python V2.22 documentation");
     }
 });
